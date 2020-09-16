1aa0bcdf6d7a1c74057f539f85a3f11241d495b2
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-16 16:05:45 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *Main - returns a message
+ *
+ *Description: prints a message then returns 0
+ *
+ *Return: 0
+ */
+int main(void)
+{ 
+printf("with proper grammar, but the outcome is a piece of art,"/n);
+return(0);
+}
