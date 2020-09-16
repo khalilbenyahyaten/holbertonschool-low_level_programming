ba82996266d59e21c31e99450282990452fdb872
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-16 15:20:21 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@ This function prints a message and then returns the value 0
 */
 int main(void)
 {
-  puts ("Programming is like building a multilingual puzzle");
+  puts ("Programming is like building a multilingual puzzle"\n);
   return(0);
 }
