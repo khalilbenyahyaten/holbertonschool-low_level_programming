9884b741af3c068e374b4124290f050722f77da8
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-16 15:47:03 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 /*
  *this program prints a message then returns 0
  */
-
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
- 
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
