64412eb96bf2057c524706c21a87a086450dacc9
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-16 18:54:39 +0000

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - function
+ *
+ *Description: prints a message then returns 1
+ *
+ *Return: 1
+ *
+ */
+int main(void)
+{
+write("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
