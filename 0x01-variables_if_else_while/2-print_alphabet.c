6e7f712819a52b993169c6c12419776cf2594521
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-17 15:02:32 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
