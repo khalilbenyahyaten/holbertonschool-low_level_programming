5a68f515e71d89061a5b759302df0fd025d24a86
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-17 19:09:45 +0000

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+*main - function main
+*
+*Description: prints all combinations of one digit numbers
+*
+*Return: 0
+*
+*/
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9' ; i++)
+{
+if (i != 0)
+{
+putchar(',');
+putchar(' ');
+}
+putchar(i);
+}
+return (0);
+}
