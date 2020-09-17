c0f5ad7f55c3593e6fd230a6b5a07d21e13736e6
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-17 18:03:52 +0000

    base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+*main - function main
+*
+*Description - prints all digits of base 10 with putchar
+*
+*Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for(i = 48; i <= 57; i++)
+{   
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
