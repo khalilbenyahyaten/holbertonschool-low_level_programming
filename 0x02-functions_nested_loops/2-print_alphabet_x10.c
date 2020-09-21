ab926800d567ac01255abe2d9fbc672d5ea28461
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-21 19:34:16 +0000

    lower case alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * main - main function
+ *
+ *Description: prints the alphabet in lowercase 10 times
+ *
+ *Return: Returns void
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+
+int i;
+int j;
+
+for (i = 1; i <= 10 ; i++)
+{
+j = 'a';
+while (j <= 'z')
+{
+_putchar(j);
+j++;
+}
+_putchar('\n');
+}
+return;
+}
