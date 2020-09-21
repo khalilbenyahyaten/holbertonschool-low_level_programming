e01e2523cfb0d453c798fb2087e13bfb251a4121
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-21 11:38:27 +0000

    holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+*main - main function
+*
+*Description: prints a message
+*
+*
+*Return: returns 0
+*
+*
+*/
+int main(void)
+{
+putchar ('H');
+putchar ('o');
+putchar ('l');
+putchar ('b');
+putchar ('e');
+putchar ('r');
+putchar ('t');
+putchar ('o');
+putchar ('n');
+putchar ('\n');
+return (0);
+}
