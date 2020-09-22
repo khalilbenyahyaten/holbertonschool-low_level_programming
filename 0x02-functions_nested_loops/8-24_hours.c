bc1fb714d1e13c254c59189f351c9e95acc61c40
khalilbenyahyaten <2300@holbertonschool.com>
2020-09-22 17:08:04 +0000

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *jack_bauer - main function
+ *
+ *Description: prints every minute of a day
+ *
+ *Return: void
+ *
+ */
+void jack_bauer(void)
+{
+int i, j;
+for (i = 0; i <= 24; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+_putchar (i / 10 + '0');
+_putchar (i % 10 + '0');
+_putchar (':');
+_putchar (j / 10 + '0');
+_putchar (j % 10 + '0');
+_putchar (' ');
+}
+}
+return;
+}
