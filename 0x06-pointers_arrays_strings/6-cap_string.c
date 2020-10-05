2f8c33476b6775ea4a6f8f6fb7f30518e9727605
khalilbenyahyaten <2300@holbertonschool.com>
2020-10-05 00:01:27 +0000

    capitalize all words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,13 +10,26 @@
  */
 char *cap_string(char *str)
 {
-int i;
-
+int i, j;
+char ch[] = " \t\n,;.!?\"(){}";
 for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] >= 'a' && str[i] <= 'z' && str[i - 1] == ' ')
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+if (i == 0)
 {
 str[i] = str[i] - 32;
+}
+else
+{
+for (j = 0; ch[j] != '\0'; j++)
+{
+if (ch[j] == str[i - 1])
+{
+str[i] = str[i] - 32;
+}
+}
+}
 }
 }
 return (str);
