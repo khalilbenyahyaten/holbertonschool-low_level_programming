8fb14643b2f795b76da535b028e68f5867799ff4
khalilbenyahyaten <2300@holbertonschool.com>
2020-10-11 15:48:19 +0000

    prints the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ *
+ *
+ *@argc: integer
+ *@argv: array of strings
+ *
+ *
+ *Return: integer
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
