77ae19a1dbdcc5711a5e779cafbd22b29b438d96
khalilbenyahyaten <2300@holbertonschool.com>
2020-10-20 13:34:27 +0000

    prints struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -22,14 +22,7 @@ else
 {
 printf("name: %s\n", d->name);
 }
-if (d->age == 0)
-{
-printf("age: (nil)\n");
-}
-else
-{
 printf("age: %f\n", d->age);
-}
 if (d->owner == NULL)
 {
 printf("owner: (nil)\n");
