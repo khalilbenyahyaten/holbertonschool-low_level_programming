aeec43c58b0342943a3004d1d893070b76ed4e34
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-01 18:58:13 +0000

    prints lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
  */
 size_t print_list(const list_t *h)
 {
-int i = 0;
+size_t i = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
@@ -22,7 +22,7 @@ else
 {
 printf("[%d] %s\n", h->len, h->str);
 }
-h = (*h).next;
+h = h->next;
 i++;
 }
 return (i);
