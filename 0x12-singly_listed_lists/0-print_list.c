ab3e78e46703c09d842e8c362df381cfd66b0095
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-01 18:26:39 +0000

    prints list

---
+++ 0x12-singly_listed_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints alla elements of a list
+ *
+ *@h: list
+ *
+ *Return: number of nodes of list h
+ */
+size_t print_list(const list_t *h)
+{
+int i = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+h = (*h).next;
+i++;
+}
+return (i);
+}
