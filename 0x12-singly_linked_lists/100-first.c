f49b2374abbe3dbc5cb99cfe71751175d4f5a969
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-02 20:17:53 +0000

    prints a string before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * fn - prints a message before the main function
+ */
+void fn(void)
+{
+char *str = "! and yet, you must allow,\nI bore my house upon my back!\n";
+
+printf("You're beat%s", str);
+}
