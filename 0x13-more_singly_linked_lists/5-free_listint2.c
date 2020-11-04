1c15f7f351018cbbf55056c4bf2fa644a8821605
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-04 19:57:59 +0000

    sets head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint2 - sets head to NULL
+ *
+ *@head: list
+ *
+ *Return: void
+ */
+void free_listint2(listint_t **head)
+{
+*head = NULL;
+return;
+}
