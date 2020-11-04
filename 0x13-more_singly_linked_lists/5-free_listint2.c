6712a47d011c46973c779fca7621c0408a77869d
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-04 20:05:20 +0000

    sets head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,6 +10,15 @@
  */
 void free_listint2(listint_t **head)
 {
+listint_t *ten, *fr;
+
+ten = *head;
+while (head != NULL && ten != NULL)
+{
+fr = ten;
+ten = ten->next;
+free(fr);
+}
 *head = NULL;
 return;
 }
