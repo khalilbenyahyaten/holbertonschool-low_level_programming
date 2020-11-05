d0efedda9838aa843a4664e68f00f8bbe8d4401d
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-05 21:33:17 +0000

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,28 +11,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-  int i, k, j, x = 0;
+  int j, i, k, x = 0;
   unsigned int n = 0;
-  char *str = "";
   if (b == NULL)
     return (0);  
 for (i = 0; b[i] != '\0'; i++)
     {
-      if (b[i] < '0' || b[i] > '9')
+      if (b[i] != '0' || b[i] != '1')
 	return (0);
     }
-  j = i - 1;
-  for (i = 0; b[i] != '\0'; i++)
-    {
-      str[0] = b[i];
-      str[1] = '\0';
-    x = atoi(str);
-      for (k = 0; k < j; k++)
-	{
-	x = x * x;
-	}
-      n += x;
-      j -= 1;
-    }
-  return (n);
+ i--;
+ for (j = 0; b[j] != '\0'; j++)
+   {
+     if (b[j] == '1')
+       {
+	 x = 1;
+	 k = 0;	 
+	 while (k < i)
+	   {
+	     x = x * 2;
+	     k++;
+	   }
+       }
+     i--;
+     n = n + x;
+   }
+ return (n);
 }
