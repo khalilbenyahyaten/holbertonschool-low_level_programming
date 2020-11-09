ca860d4124192be17a7a73f5aa54236d9472a324
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-09 17:23:43 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int fd, r, i = 0;
 if (filename == NULL)
 return (-1);
 fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
-if (fd == 0)
+if (fd == -1)
 return (-1);
 if (text_content != NULL)
 {
@@ -28,9 +28,8 @@ while (text_content[i] != '\0')
 i++;
 }
 r = write(fd, text_content, i);
-if (r == 0)
+if (r == -1)
 {
-close(fd);
 return (-1);
 }
 close(fd);
