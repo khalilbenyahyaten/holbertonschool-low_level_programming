d8a58e2e84982ac03f61180896e5d5e1838ba0d7
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-09 16:25:07 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,41 +6,34 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 /**
+ * create_file - create a file
  *
+ *@filename: file name
+ *@text_content: text to be written in file
  *
- *
- *
- *
- *
- *
+ *Return: -1 if failed or 1 in case of success
  */
 int create_file(const char *filename, char *text_content)
 {
-  int fd, r, i;
+int fd, r, i = 0;
 
-
-  if (filename == NULL)
-    return (-1);
-  fd = open(filename, O_TRUNC | O_CREAT, 0600);
-  if (fd == -1)
-    return (-1);
-
-  for (i = 0; text_content[i]; i++)
-    {
-    }
-  if (i == 0)
-    {
-    close(fd);
-    return (1);
-    }  
-r = read(fd, text_content, i);
-  close(fd);  
-if (r != -1)
-    {
-    return (1);
-    }
-  else
-    {
-    return (-1);
-    }
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+if (fd == -1)
+return (-1);
+while (text_content[i] != '\0')
+i++;
+if (i == 0)
+{
+close(fd);
+return (1);
+}
+r = write(fd, text_content, i);
+close(fd);
+if (r == -1)
+{
+return (-1);
+}
+return (1);
 }
