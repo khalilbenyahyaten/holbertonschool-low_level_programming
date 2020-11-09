76ca344b1d89be95611b893aa7483436b9e753e5
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-09 12:53:50 +0000

    header fiel

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,3 +1,9 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #ifndef HEADER
 #define HEADER
 ssize_t read_textfile(const char *filename, size_t letters);
