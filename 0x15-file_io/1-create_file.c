8a3914e10055dbb03577789211c3948f3f66cbfc
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-09 15:47:16 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,10 +21,11 @@ int create_file(const char *filename, char *text_content)
 
   if (filename == NULL)
     return (-1);
-  fd = open(filename, O_CREAT, 0600);
+  fd = open(filename, O_TRUNC | O_CREAT, 0600);
   if (fd == -1)
     return (-1);
-  for (i = 0; text_content[i] != '\0'; i++)
+
+  for (i = 0; text_content[i]; i++)
     {
     }
   if (i == 0)
