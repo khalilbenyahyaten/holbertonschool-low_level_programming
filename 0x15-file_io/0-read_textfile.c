8df4928e02c907fbe73c5e53c690a184aea09bd5
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-09 14:16:55 +0000

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+B#include "holberton.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -6,30 +6,37 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 /**
+ * read_textfile - reads from a text file
  *
+ *@filename: file to read from
+ *@letters:
  *
- *
- *
- *
- *
- *
- *
- *
+ *Return: the number of letters read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
-  char *buf = "";
-  
+int fd;
+ssize_t r, w;
+char *buf;
+
 if (filename == NULL)
-    return (0);
-
- fd = open("filename", O_WRONLY);
-  if (fd == 0)
-    return (0);
-
-  read(fd, buf, letters);
-  printf("%s\n", buf);  
-  close(fd);
-return(letters);
+{
+return (0);
+}
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+return (0);
+}
+r = read(fd, buf, letters);
+if (r == -1)
+return (0);
+close(fd);
+w = write(STDOUT_FILENO, buf, r);
+if (r != w)
+return (0);
+return (w);
 }
