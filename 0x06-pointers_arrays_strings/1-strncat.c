eacc15b4c94e7c0a742ddede6e5c2ea4db376d4b
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-13 11:09:23 +0000

    concats two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,11 +15,18 @@ int i, j, k;
 for (k = 0; src[k] != '\0'; k++)
 {
 }
-if (k > n)
-{
 for (i = 0; dest[i] != '\0'; i++)
 {
 }
+if (k > n)
+{
+for (j = 0; j != n; j++)
+{
+dest[i] = src[j];
+}
+}
+else if (k < n)
+{
 for (j = 0; j != k; j++)
 {
 dest[i] = src[j];
