8ea53eb20cb331eeb2eb258d3a9e997df6e52098
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-13 10:53:19 +0000

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * *_strncat - concatenates two strings
+ *
+ *@dest: destination
+ *@src: source
+ *@n: int
+ *
+ *Return: destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (j = 0; j != n; j++)
+{
+dest[i] = src[j];
+}
+return (dest);
+}
