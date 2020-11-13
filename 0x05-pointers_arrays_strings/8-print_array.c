2fe7474bfb6b6907ef5b394863751bc966b27985
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-13 13:34:56 +0000

    prints n elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,15 @@ void print_array(int *a, int n)
 int i;
 char *sep = ", ";
 
-for (i = 0; i < n - 1; i++)
+for (i = 0; i < n; i++)
+{
+if (i < n - 1)
 {
 printf("%d%s", a[i], sep);
 }
+else
+{
 printf("%d\n", a[i]);
+}
+}
 }
