6be467804e9f12df399476ff04824e27fbe99220
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-16 16:37:34 +0000

    concats two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,11 +13,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i, j;
 char *str;
+
+if (s1 == NULL)
+i = 0;
+else
+{
 for (i = 0; s1[i] != '\0'; i++)
 {
 }
+}
+if (s2 == NULL)
+j = 0;
+else
+{
 for (j = 0; s2[j] != '\0'; j++)
 {
+}
 }
 if (j < n)
 {
