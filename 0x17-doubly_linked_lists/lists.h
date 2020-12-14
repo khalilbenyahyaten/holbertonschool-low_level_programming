74d2f90c6ec8ec84cbf77460bc27c13c5bb74bcb
khalilbenyahyaten <2300@holbertonschool.com>
2020-12-14 12:06:34 +0000

    header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -23,8 +23,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
  */
 typedef struct dlistint_s
 {
-  int n;
-  struct dlistint_s *prev;
-  struct dlistint_s *next;
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
 } dlistint_t;
 #endif
