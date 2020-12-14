24e5c3977cae13ae076ba046f730fa68fad395ac
khalilbenyahyaten <2300@holbertonschool.com>
2020-12-14 15:08:12 +0000

    frees a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+B#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - frees a list
+ *
+ *@head: list
+ *
+ *Return: void
+ *
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *fr, *ten;
+ten = head;
+while (ten != NULL)
+{
+fr = ten->next;
+free(ten);
+ten = fr;
+}
+}
