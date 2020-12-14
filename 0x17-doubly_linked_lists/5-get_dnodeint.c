224d944654302c39cf3ae7affb243f1d7889d80b
khalilbenyahyaten <2300@holbertonschool.com>
2020-12-14 16:22:51 +0000

    returns the node at index position

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * *get_dnodeint_at_index - gets the node at index
+ *
+ *@head: list
+ *@index: index
+ *
+ *Return: node at index position
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+
+while (i < index)
+{
+head = head->next;
+i++;
+}
+if (i < index)
+return (NULL);
+return (head);
+}
