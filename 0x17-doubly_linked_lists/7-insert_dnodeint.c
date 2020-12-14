b177ec5d8dde8f8f1814047b57469e83c1068e96
khalilbenyahyaten <2300@holbertonschool.com>
2020-12-14 16:34:38 +0000

    inserts node at a given position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * *insert_dnodeint_at_index - inserts a node at a given position
+ *
+ *@h: list
+ *@idx: index
+ *@n: integer
+ *
+ *Return: position of the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}
