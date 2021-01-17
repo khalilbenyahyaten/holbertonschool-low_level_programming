cbc6936c49540b73116168097ae7d44366ec404d
khalilbenyahyaten <23000@holbertonschool.com>
2021-01-17 19:50:39 +0000

    get value associated with key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieve a value associated with a key
+ *
+ *@ht: hash table
+ *@key: key
+ *
+ *Return: value or null
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int i;
+hash_node_t *n;
+
+if (!ht || !key)
+return (NULL);
+i = key_index((const unsigned char *)key, ht->size);
+n = ht->array[i];
+while (n)
+{
+if(strcmp(node->key, key) == 0)
+{
+return (n->value);
+}
+n = n->next;
+}
+return (NULL);
+}
